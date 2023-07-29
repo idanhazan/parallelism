b90b80abae1acf607b0ec2bd989e5b2bda1d6440
Idan Hazan <as4ny0o@gmail.com>
2023-07-29 09:57:51 +0300

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+import parallelism
+
+project = 'parallelism'
+author = 'Idan Hazan'
+copyright = f'2023, {author}'
+html_theme = 'sphinx_rtd_theme'
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
